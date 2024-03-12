fc1f76f75898de6e62547f2ea531011940713190
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-12 17:07:20 -0300

    feat: create place-form component

---
+++ src/components/place-form/place-form.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function PlaceForm() {
+   return (
+      <View>
+         <Text>The Place form</Text>
+      </View>
+   );
+}
