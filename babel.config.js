6ae676fa527eab1658dacb6c6ce4f36e12929a98
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-12 20:46:52 -0300

    feat: create global colors

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,7 @@ module.exports = function (api) {
                   '@models': './src/models',
                   '@typings': './src/typings',
                   '@routes': './src/routes',
+                  '@constants': './src/constants',
                },
             },
          ],
