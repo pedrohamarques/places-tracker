a6918947734b0d9cee19ef1f698e0a73102ef7df
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-12 20:57:16 -0300

    feat: add styles to stack navigator

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import AllPlacesScreen from '@screens/all-places';
 import AddPlaceScreen from '@screens/add-place';
 
+import { Colors } from '@constants/colors';
+
 import { StackRoutes, type RoutesParams } from './types';
 
 const Stack = createNativeStackNavigator<RoutesParams>();
@@ -12,14 +14,25 @@ const Stack = createNativeStackNavigator<RoutesParams>();
 export function Routes() {
    return (
       <NavigationContainer>
-         <Stack.Navigator>
+         <Stack.Navigator
+            screenOptions={{
+               headerStyle: { backgroundColor: Colors.primary500 },
+               headerTintColor: Colors.gray700,
+               contentStyle: { backgroundColor: Colors.gray700 },
+            }}>
             <Stack.Screen
                name={StackRoutes.ALL_PLACES}
                component={AllPlacesScreen}
+               options={{
+                  title: 'Your favorites places',
+               }}
             />
             <Stack.Screen
                name={StackRoutes.ADD_PLACES}
                component={AddPlaceScreen}
+               options={{
+                  title: 'Add a new place',
+               }}
             />
          </Stack.Navigator>
       </NavigationContainer>
